9d106f94e7a0aa2d39fb250959309af7ddf3ae9d
imenov06 <imenov06@gmail.com>
2024-04-24 19:42:05 +0300

    four

--- app/methodologies/router.py
+++ app/methodologies/router.py
@@ -1,6 +1,6 @@
 import time
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from app.methodologies.utils import get_methodology_with_images, get_all_methodologies_with_images
 from app.methodologies.schemas import SMethod
@@ -14,6 +14,8 @@ router = APIRouter(
 @router.get("/get/{model_id}")
 @cache(expire=60)
 def get_methodology_by_id(model_id: int) -> SMethod:
+    if model_id <= 0 or model_id is not int:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ID")
     schema_method = get_methodology_with_images(model_id)
     return schema_method
 
